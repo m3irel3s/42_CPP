3f5b3cc3431ad5210838626f321ea2fe4d89e196
m3irel3s <miguelmeireles025@gmail.com>
2025-05-06 19:59:16 +0100

    finished ex04

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 	std::ifstream file(argv[1]);
 	if (!file.is_open())
 	{
-		std::cout << "Invalid file given" << std::endl;
+		std::cout << "Error opening file" << std::endl;
 		return 1;
 	}
 	Replace(file, argv);
@@ -20,7 +20,28 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-void	Replace(std::ifstream &file, char **argv)
+void Replace(std::ifstream &file, char **argv)
 {
+	std::string outFileName = std::string(argv[1]) + ".replace";
+	std::ofstream outfile(outFileName.c_str());
 
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::string line;
+
+	while (std::getline(file, line))
+	{
+		std::string result;
+		std::size_t pos = 0;
+		std::size_t found;
+
+		while ((found = line.find(s1, pos)) != std::string::npos)
+		{
+			result += line.substr(pos, found - pos);
+			result += s2;
+			pos = found + s1.length();
+		}
+		result += line.substr(pos);
+		outfile << result << std::endl;
+	}
 }
